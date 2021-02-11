97765a7928b69e073a21db3525d4ad5a7859e6ab
Felicia Heiney <bhfelicia@gmail.com>
2021-02-10 21:35:07 -0500

    displays all literary works by each author in the database

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const client = new pg.Client("postgres://localhost/book_world_db");
 
 const syncAndSeed = async () => {
   const SQL = `
+  DROP TABLE IF EXISTS translator;
   DROP TABLE IF EXISTS book;
   DROP TABLE IF EXISTS author;
   CREATE TABLE author(
@@ -13,9 +14,28 @@ const syncAndSeed = async () => {
 
   CREATE TABLE book(
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+    original_title VARCHAR(100) NOT NULL,
+    english_title VARCHAR(100) NOT NULL,
+    language VARCHAR(100) NOT NULL,
+    genre VARCHAR(100),
+    translator VARCHAR(100),
     author_id INTEGER REFERENCES author(id)
   );
+
+
+  INSERT INTO author(name) VALUES('Marcel Proust');
+  INSERT INTO author(name) VALUES('Leo Tolstoy');
+  INSERT INTO author(name) VALUES('Jorge Luis Borges');
+
+  INSERT INTO book(original_title, english_title, language, genre, translator, author_id) VALUES('À la recherche du temps perdu', 'Remembrance of Things Past', 'French', 'Modernist Novel', 'C. K. Scott Moncrieff and Terence Kilmartin', 1);
+
+  INSERT INTO book(original_title, english_title, language, genre, translator, author_id) VALUES('Анна Каренина', 'Anna Karenina', 'Russian', 'Realist Novel', 'Louise and Aylmer Maude', 2);
+
+  INSERT INTO book(original_title, english_title, language, genre, translator, author_id) VALUES('Война и мир', 'War and Peace', 'Russian', 'Philosophical Novel', 'Nathan Haskell Dole', 2);
+
+  INSERT INTO book(original_title, english_title, language, genre, translator, author_id) VALUES('Ficciones', 'Fictions', 'Spanish', 'Short Story Collection', 'Anthony Kerrigan, Anthony Bonner, Alastair Reid, Helen Temple, and Ruthven Todd', 3);
+
+
   `;
   await client.query(SQL);
 };
--- server.js
+++ server.js
@@ -6,10 +6,70 @@ const html = require("html-template-tag");
 
 const app = express();
 
-app.use("assets", express.static(path.join(__dirname, "/assets/styles.css")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/", async (req, res, next) => {
   try {
+    const authorRes = await client.query(`SELECT * FROM author;`);
+    const authors = authorRes.rows;
+    res.send(html`
+      <html>
+        <head>
+          <link rel="stylesheet" href="/assets/styles.css" />
+        </head>
+        <body>
+          <h1>Works in Translation that Shaped the Literary Landscape</h1>
+          <h2>Authors</h2>
+          <ul>
+            ${authors.map(
+              (author) => `
+      <li>
+      <a href='/authors/${author.id}'>
+      ${author.name}
+      </a>
+      </li>
+      `
+            )}
+          </ul>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/authors/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const bookRes = await client.query(
+      `SELECT * FROM book JOIN author ON book.author_id = author.id WHERE book.author_id = $1`,
+      [id]
+    );
+    const books = bookRes.rows;
+
+    res.send(html`
+      <html>
+        <head>
+          <link rel="stylesheet" href="/assets/styles.css" />
+        </head>
+        <body>
+          <h1>Literary works by ${books[0].name}</h1>
+          ${books.map(
+            (book) => html` <div class="book">
+              <h3>${book.english_title}</h3>
+              <div>A ${book.genre}</div>
+              <div class="original">
+                Originally ${book.original_title} in ${book.language}
+              </div>
+              <div>Translated into English by ${book.translator}</div>
+              <div></div>
+            </div>`
+          )}
+          <h3><a href="/">Back to All Authors</a></h3>
+        </body>
+      </html>
+    `);
   } catch (error) {
     next(error);
   }
